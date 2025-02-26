9af202943f0d2ceeafb301964046dbc86c0668df
Your Name <you@example.com>
2025-02-26 16:38:22 -0600

    Third

--- shop/item.js
+++ shop/item.js
@@ -2,8 +2,11 @@ let cart = JSON.parse(localStorage.getItem("cart_cache"))
 
 if (!cart) cart = [];
 
-let sku = document.getElementById("sku").innerText.replace(/\D/g,'');
+const sku = document.getElementById("sku").innerText.replace(/\D/g,'');
 
 function cart_add_sku() {
-    
+    console.log(sku)
 }
+
+const button = document.getElementById("add_to_cart")
+button.onclick = cart_add_sku
