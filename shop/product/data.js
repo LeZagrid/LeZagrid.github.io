aaaecd1384feda9ba70595d771df56357b68f7bf
Your Name <you@example.com>
2025-02-25 20:10:50 -0600

    Initial

---
+++ shop/product/data.js
@@ -0,0 +1,7 @@
+data = [
+    {
+        "id": 1,
+        "image_url": "https://www.fallstruckcenter.com/product-page/united-pacific-carabiner",
+        "name": "United Pacific Carabiner"
+    }
+]
