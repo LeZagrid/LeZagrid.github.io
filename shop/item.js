e38d9f9035ee8fb0f742e0d682e735ae497ea3d0
Your Name <you@example.com>
2025-02-26 23:37:55 -0600

    Fourth

--- shop/item.js
+++ shop/item.js
@@ -6,7 +6,25 @@ const sku = document.getElementById("sku").innerText.replace(/\D/g,'');
 
 function cart_add_sku() {
     console.log(sku)
+    if (cart.length == 0) 
+        cart.push(item)
+    else { 
+        let exist = cart.filter(_item => item.id == _item.id)
+        if (exist.length != 0) {
+            cart.find(_item =>  _item.quantity += 1)
+        } else {
+            cart.push(item)
+        }
+     }
 }
 
-const button = document.getElementById("add_to_cart")
-button.onclick = cart_add_sku
+function quantity() {
+    let quantity = 0;
+    const _cart = JSON.parse(localStorage.getItem("cart_cache")) 
+    _cart.forEach(item => quantity += item.quantity)
+    console.log("quantity", quantity)
+    localStorage.setItem("cart_quantity", quantity)
+    cart_quantity()
+}
+
+quantity()
